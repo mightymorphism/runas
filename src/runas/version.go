3aab244efea7a1033f6dee558105277ad866ce3f
William Josephson <wkj@users.noreply.github.com>
2017-12-13 15:36:42 -0500

    Bump version number for -reexec

--- src/runas/version.go
+++ src/runas/version.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 Trough Creek Holdings, LLC.  All Rights Reserved
 package main
 
-const BuildRelease = "0.0.1"
-const BuildRevision = "7658a2895cf1ecfe5bb2476b4e0c939e41f2a673"
-const BuildDate = "2017-11-24 18:13:44 -0500"
+const BuildRelease = "0.0.2"
+const BuildRevision = "f50841067515e064c8b35d37695f0b8286b33c5e"
+const BuildDate = "2017-12-13 15:34:38 -0500"
