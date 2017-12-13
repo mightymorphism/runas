cbf6820fd24f823822278cb4e6ae5f66cbe74175
William Josephson <wkj@users.noreply.github.com>
2017-12-13 15:56:11 -0500

    Bump version for change in default re-exec behavior

--- src/runas/version.go
+++ src/runas/version.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 Trough Creek Holdings, LLC.  All Rights Reserved
 package main
 
-const BuildRelease = "0.0.2"
-const BuildRevision = "f50841067515e064c8b35d37695f0b8286b33c5e"
-const BuildDate = "2017-12-13 15:34:38 -0500"
+const BuildRelease = "0.0.3"
+const BuildRevision = "7ad1fef10e8f6102cac5a200e1be2b456947b907"
+const BuildDate = "2017-12-13 15:55:25 -0500"
