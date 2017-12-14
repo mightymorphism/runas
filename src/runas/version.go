b156e7f54e38f136aac7bd6b5efec73c37b375fc
William Josephson <wkj@users.noreply.github.com>
2017-12-13 19:23:39 -0500

    Bump version for latest release.

--- src/runas/version.go
+++ src/runas/version.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 Trough Creek Holdings, LLC.  All Rights Reserved
 package main
 
-const BuildRelease = "0.0.3"
-const BuildRevision = "7ad1fef10e8f6102cac5a200e1be2b456947b907"
-const BuildDate = "2017-12-13 15:55:25 -0500"
+const BuildRelease = "0.0.5"
+const BuildRevision = "de603a3c0514eb7d81bee45f58d2317b4bf6575f"
+const BuildDate = "2017-12-13 21:26:57 -0500"
