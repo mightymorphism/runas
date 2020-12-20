3cc217b4fd749ff11a9427e547a8dd7dbf14273b
William Josephson <wkj@morphisms.net>
2020-12-20 18:14:07 -0500

    Bump release version to 0.0.11.

--- src/runas/version.go
+++ src/runas/version.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2017, 2018 Trough Creek Holdings, LLC.  All Rights Reserved.
 package main
 
-const BuildRelease = "0.0.10"
-const BuildRevision = "bfc65bf7f5d448be685b6a6a93fd73d59299a873"
-const BuildDate = "2019-03-05 16:45:33 -0500"
+const BuildRelease = "0.0.11"
+const BuildRevision = "5ffa01bc3f2bf2686f44f8c629d47a7fa661ee29"
+const BuildDate = "2020-12-19 12:28:33 -0500"
